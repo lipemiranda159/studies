8490d51583b88c44f03e41f58f01c3d63a01f6ea
felipem <felipem@takenet.com.br>
2016-06-21 13:28:48 -0300

    Add ninject to the project

--- BookStore/BookStore/Models/Repository.cs
+++ BookStore/BookStore/Models/Repository.cs
@@ -5,13 +5,29 @@ using System.Web;
 
 namespace BookStore.Models
 {
-    public class Repository
+    public class Repository : IRepository
     {
         private BookStoreContext db;
 
         public Repository(BookStoreContext db)
         {
             this.db = db;
+        }
+
+        public IQueryable<Order> GetAllOrders()
+        {
+            return db.Orders;
+        }
+
+        public IQueryable<Order> GetAllOrdersWithDetails()
+        {
+            return db.Orders.Include("OrderDetails");
+        }
+
+        public Order GetOrder(int id)
+        {
+            return db.Orders.Include("OrderDetails")
+                .FirstOrDefault(o => o.Id == id);
         }
     }
 }
