c61a4f53839ca7f3ca3c8ee8e72262928f8b2956
felipem <felipem@takenet.com.br>
2016-06-21 13:12:19 -0300

    Create db repository

---
+++ BookStore/BookStore/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Repository
+    {
+        private BookStoreContext db;
+
+        public Repository(BookStoreContext db)
+        {
+            this.db = db;
+        }
+    }
+}
