a666a013562f2a0f8c28566c949f99437cb49801
felipem <felipem@takenet.com.br>
2016-06-21 11:40:09 -0300

    Add models class

---
+++ BookStore/BookStore/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
