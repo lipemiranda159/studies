e526a2b17dc93bd16208dca3edf0dbadcdfe5098
lipemiranda159 <lipemiranda159@hotmail.com>
2016-06-23 00:25:24 -0300

    Create checkOut time project

---
+++ CheckoutApi/CheckoutApi/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace CheckoutApi.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ CheckoutApi/CheckoutApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheckoutApi.WebApiApplication" Language="C#" %>
