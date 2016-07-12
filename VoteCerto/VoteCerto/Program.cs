e5ed85b60a32a36555af57fd99ad3a11b95f1b2e
felipem <felipem@takenet.com.br>
2016-07-12 13:16:00 -0300

    Create project for bot V0t3C3rt0

---
+++ VoteCerto/VoteCerto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Transparencia.Brasil.SDK.Entities;
+using Transparencia.Brasil.SDK.Resources;
+
+namespace VoteCerto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var candidatos = new Candidatos();
+
+                var listCandidatos = candidatos.Count(10);
+
+                foreach (var item in listCandidatos)
+                {
+                    Console.WriteLine(string.Format("id: {0} - name: {1} - miniBio: {2}", item.id, item.nome, item.miniBio));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
