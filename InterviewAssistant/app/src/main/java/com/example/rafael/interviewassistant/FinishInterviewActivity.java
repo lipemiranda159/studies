04655a286e684596e4570f2d36c1fca2b3e0b9cf
Rafael Miranda <Rafael Miranda>
2017-01-18 22:30:53 -0200

    move files

---
+++ InterviewAssistant/app/src/main/java/com/example/rafael/interviewassistant/FinishInterviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.rafael.interviewassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinishInterviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_interview);
+    }
+}
--- InterviewAssistant/app/src/main/java/com/example/rafael/interviewassistant/VerifyAgeActivity.java
+++ InterviewAssistant/app/src/main/java/com/example/rafael/interviewassistant/VerifyAgeActivity.java
@@ -1,7 +1,9 @@
 package com.example.rafael.interviewassistant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VerifyAgeActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class VerifyAgeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_age);
     }
+
+    public void onRadioYesClicked(View view){
+
+        Intent activity = new Intent(this, ApresentationActivity.class);
+        startActivity(activity);
+    }
+
+    public void onRadioNoClicked(View view)
+    {
+        Intent activity = new Intent(this, FinishInterviewActivity.class);
+        startActivity(activity);
+    }
+
 }
