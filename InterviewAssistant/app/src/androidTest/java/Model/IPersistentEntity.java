068a5afad0d684e37ca8f61ce6a578a94a58cfed
Rafael Miranda <Rafael Miranda>
2017-01-19 19:06:26 -0200

    create generic entity

--- InterviewAssistant/app/src/androidTest/java/Model/IPersistentEntity.java
+++ InterviewAssistant/app/src/androidTest/java/Model/IPersistentEntity.java
@@ -4,7 +4,7 @@ package Model;
  * Created by Rafael on 19/01/2017.
  */
 
-public interface PersistentEntity {
+public interface IPersistentEntity {
     public int getId();
     public void setId(int id);
 }
