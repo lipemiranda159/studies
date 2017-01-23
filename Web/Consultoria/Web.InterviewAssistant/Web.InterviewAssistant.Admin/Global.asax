d0dca598ef5705cac8ba0500a2b249f8e5ba5863
Rafael Miranda <Rafael Miranda>
2017-01-23 02:01:21 -0200

    use admin interface

---
+++ Web/Consultoria/Web.InterviewAssistant/Web.InterviewAssistant.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web.InterviewAssistant.Admin.MvcApplication" Language="C#" %>
---
+++ Web/Consultoria/Web.InterviewAssistant/Web.InterviewAssistant.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.InterviewAssistant.Admin.Startup))]
+namespace Web.InterviewAssistant.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
